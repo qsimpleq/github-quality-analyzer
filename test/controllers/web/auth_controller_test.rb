e22069b806e4c88503edc8637627bf1243672821
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-08-25 20:01:06 +0500

    Update AuthControllerTest

--- test/controllers/web/auth_controller_test.rb
+++ test/controllers/web/auth_controller_test.rb
@@ -1,44 +1,33 @@
 # frozen_string_literal: true
 
+require 'test_helper'
+
 module Web
   class AuthControllerTest < ActionDispatch::IntegrationTest
     setup do
       @user1 = users(:user1)
     end
 
-    test 'should github auth' do
+    test '#request' do
       post auth_request_path('github')
 
       assert_response :redirect
     end
 
-    test 'create' do
-      auth_hash = {
-        info: {
-          email: @user1.email,
-          nickname: @user1.nickname
-        },
-        credentials: {
-          token: 'fake'
-        }
-      }
+    test '#callback' do
+      sign_in(@user1)
 
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
-
-      get callback_auth_path('github')
-
-      assert_response :redirect
-
-      user = User.find_by!(email: auth_hash[:info][:email].downcase)
-
-      assert user
+      assert_redirected_to root_path
+      assert User.find(@user1.id)
       assert { signed_in? }
+      assert_equal flash[:notice], t('.sign_in')
     end
 
-    test 'should destroy' do
+    test '#destroy' do
       delete auth_logout_path
 
       assert_response :redirect
+      assert_equal flash[:notice], t('.sign_out')
     end
   end
 end
